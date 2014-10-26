0693295e63b219a3ddef9dfefba7d58b508e00ed
Piotr Rogoża <rogoza.piotr@gmail.com>
2014-10-26 23:02:07 +0100

    Added handle event keypup Improved view

--- js/count.js
+++ js/count.js
@@ -4,6 +4,7 @@
 (function($) {
     $.extend({
         count_distance: function () {
+            // input
             var distance    = parseFloat( $("#distance").val().replace(",",".").replace(" ","") ) || 0;
             var days        = parseFloat( $("#days").val() ) || 0;
             var hours       = parseFloat( $("#hours").val() ) || 0;
@@ -14,25 +15,34 @@
             var floatHours          = totalSeconds / 3600;
             var kilometersDistance  = distance/1000;
 
+            // count pace
             var secondsPerKm    = Math.round(totalSeconds/kilometersDistance);
             var minutesPerKm    = Math.floor(secondsPerKm/60);
             var rest            = secondsPerKm - (minutesPerKm*60);
 
-            var speed = Math.round(kilometersDistance/floatHours*100)/100;
-            $("input#speed").val(speed);
+            // output
             $("input#pace_minutes").val(minutesPerKm);
             $("input#pace_seconds").val(rest);
+
+            // count speed
+            var speed = Math.round(kilometersDistance/floatHours*1000)/1000;
+            $("input#speed").val(speed);
+
         },
         count_speed: function() {
+            // input
             var distance    = parseFloat( $("#distance").val().replace(",",".").replace(" ","") ) || 0;
             var speedKmH    = parseFloat( $("#speed").val().replace(",",".").replace(" ","") ) || 0;
 
             // speed -> pace
             var a = 1 / speedKmH * 60;
             var pace_minutes = Math.floor(a);
-            var pace_seconds = Math.round((a - pace_minutes) * 60);
+            var pace_seconds = ((a - pace_minutes) * 60);
+            var pace_sseconds = Math.round((pace_seconds%1)*100);
+            pace_seconds = Math.round(pace_seconds);
             $("input#pace_minutes").val(pace_minutes);
             $("input#pace_seconds").val(pace_seconds);
+            $("input#pace_sseconds").val(pace_sseconds);
 
             var speedMPerS = speedKmH/3600*1000;
             var totalSeconds = Math.round(distance/speedMPerS);
@@ -54,18 +64,20 @@
 
         },
         count_pace: function() {
+            // input
             var paceMinutes = parseFloat( $("#pace_minutes").val() ) || 0;
             var paceSeconds = parseFloat( $("#pace_seconds").val() ) || 0;
-            var distance    = parseFloat( $("#distance").val() ) || 0;
+            var distance    = parseFloat( $("#distance").val().replace(",",".").replace(" ","") ) || 0;
 
+            // pace -> speed
             var totalSeconds = paceMinutes*60 + paceSeconds;
             var d = 1/(totalSeconds/3600);
-            var speed = Math.round(d*100)/100;
+            var speed = Math.round(d*1000)/1000;
 
             $("input#speed").val(speed);
-            // TODO: w ile przebiegnę dany dystans
-            // pace*distance
-            var time = totalSeconds*distance/1000; //[s]
+
+            // pace -> time
+            var time = totalSeconds*distance/1000; // s/km
             var days = Math.floor(time/(24*3600));
             time = time%(24*3600);
             var hours = Math.floor(time/3600);
@@ -89,14 +101,30 @@ jQuery(document).ready( function($) {
         e.preventDefault();
         $.count_distance(e);
     });
+    $("#distance,#days,#hours,#minutes,#seconds").keyup(
+        function() {
+            $.count_distance();
+        }
+    );
+
     $("button#count_speed").click(function (e) {
         e.preventDefault();
         $.count_speed();
     });
+    $("#speed").keyup(
+        function () {
+            $.count_speed();
+        }
+    );
     $("button#count_pace").click(function (e) {
         e.preventDefault();
         $.count_pace();
     });
+    $("#pace_minutes,#pace_seconds").keyup(
+        function () {
+            $.count_pace();
+        }
+    );
 
     $(".btn-reset").on("click", function(){
         $(".resetThis").val("");
